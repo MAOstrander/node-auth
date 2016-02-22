54b30fbb1d351a04ad98cfff5ed92ee1a74aed83
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-02-22 11:16:48 -0600

    connect-redis now stores the paths visited

--- server.js
+++ server.js
@@ -2,11 +2,27 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
 const PORT = process.env.PORT || 3000;
+const SESSION_SECRET = process.env.SESSION_SECRET || 'secretsquirrel';
 
 app.set('view engine', 'jade')
 
 app.use(bodyParser.urlencoded( {extended: false} ) );
+app.use(session({
+  secret: SESSION_SECRET,
+  store: new RedisStore()
+}));
+app.use((req, res, next) => {
+  req.session.visits = req.session.visits || {};
+  req.session.visits[req.url] = req.session.visits[req.url] || 0;
+  req.session.visits[req.url]++;
+
+  console.log(req.session);
+  next();
+});
+
 
 
 app.get('/', (req, res) => {
