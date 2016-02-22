690d6216e6b36eea9ae9b6307a9eb27d1098f151
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-02-22 09:24:35 -0600

    using jade now

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.set('view engine', 'jade')
 
-    res.send('You found a thing!');
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 
