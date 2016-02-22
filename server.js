ab00fb08b0176872a626b27e1596b1012ea9b5f5
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-02-22 09:46:42 -0600

    got a form going

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 "use strict";
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'jade')
 
+app.use(bodyParser.urlencoded( {extended: false} ) );
+
+
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', (req, res) => {
+  console.log("Logged in now?");
+  res.redirect('/');
+});
+
 
 
 app.listen(PORT, () => {
